6c564c961b79281789d925db67732f852e4e9b41
nicholas <nicholastillo@gmail.com>
2024-04-05 22:08:47 -0400

    Before Testing

---
+++ Code/Javascript/main.js
@@ -0,0 +1,22 @@
+ function event_button(parameter){
+    // Change some html on the class event-info-parent or lorem-ipsum
+    if(parameter == 1) {
+      var elements = document.getElementsByClassName("event-name5");
+      
+
+    }
+    else if (parameter == 2) {
+
+    }
+    else if (parameter == 3) {
+
+    }
+
+    else if (parameter == 4) {
+
+    }
+ }
+
+
+
+ 
